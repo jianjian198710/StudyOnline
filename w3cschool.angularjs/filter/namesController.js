a2e38ce6373612b1a8cecf5c0d5ef7b6bd78f417
jianjian@office <jianjian@office.com>
2014-09-15 17:50:01 +0800

    WalktoHome

---
+++ w3cschool.angularjs/filter/namesController.js
@@ -0,0 +1,6 @@
+function namesController($scope){
+	$scope.names=[{name:"Hazard",country:"Belegum"},
+	              {name:"Lampard",country:"England"},
+	              {name:"Terry",country:"England"},
+	              {name:"Ramires",country:"Brazil"}];
+}
---
+++ w3cschool.angularjs/filter/personController.js
@@ -0,0 +1,6 @@
+function personController($scope){
+	$scope.person={
+		firstName:"Frank",
+		lastName:"Lampard"
+	};
+}
